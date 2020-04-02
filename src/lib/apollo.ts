8304fb1b6278abaa9efd5e9a324d28d7a148800b
Pubudu Kodikara <pubudu@leafycode.com>
2020-04-02 12:58:33 +0530

    Enable some parts

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,10 +1,9 @@
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { WebSocketLink } from "apollo-link-ws";
-import { ApolloLink } from "apollo-link";
+import { split } from "apollo-link";
 import { getMainDefinition } from "apollo-utilities";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { onError } from "apollo-link-error";
 import { GRAPHQL_ENDPOINT, GRAPHQL_WS_ENDPOINT } from "../config";
 
 // Create an http link:
@@ -21,28 +20,17 @@ const wsLink = new WebSocketLink({
 });
 
 export const apolloClient = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    ApolloLink.split(
-      // split based on operation type
-      ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-          definition.kind === "OperationDefinition" &&
-          definition.operation === "subscription"
-        );
-      },
-      wsLink,
-      httpLink
-    ),
-  ]),
+  link: split(
+    // split based on operation type
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    httpLink
+  ),
   cache: new InMemoryCache(),
 });
