2a09c50b164262bb7c2e548e6221f9b6097bd422
Pubudu Kodikara <pubudu@leafycode.com>
2020-04-04 19:17:48 +0530

    Make sinhala default 🦁

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -14,7 +14,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    fallbackLng: "en",
+    fallbackLng: "si",
     saveMissing: true,
 
     keySeparator: false, // we do not use keys in form messages.welcome
